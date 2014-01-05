50ffd6d73aeaa4c91eee5241ed92985e47120f20
ishun.shu <delta50.waseda@gmail.com>
2014-01-05 12:44:33 +0900

    fix the bug

--- Tyuren/SendEmail.pm
+++ Tyuren/SendEmail.pm
@@ -13,8 +13,8 @@ use Date::Calc qw/Today_and_Now/;
 use Mail::Sendmail;
 
 use constant {
-    LOGIN  => 1,
-    LOGOUT => 2,
+    FROM => 'admin@sakuragakusha.com',
+    CC   => 'sakuragakusha+tyuren@gmail.com',
 };
 
 my $KIND_TO_NAME = {
@@ -27,7 +27,7 @@ sub new {
     return unless $args;
     return $class->SUPER::new({
 	params     => $args,
-	from_email => 'admin@sakuragakusha.com',
+	from_email => FROM,
 	to_email   => $args->{address},
 	subject    => defined $args->{subject} ? $args->{subject} : '櫻學舎からのお知らせ',
 	body       => defined $args->{body} ? $args->{body} : '',
@@ -38,32 +38,28 @@ sub send {
     my $self = shift;
 
     if ($self->{body} eq '') {
-	if($self->{params}->{kind} == LOGIN || $self->{params}->{kind} == LOGOUT) {
-	    $self->{body} = sprintf(
-		_mail_body(),
-		$self->{params}->{nickname},
-		$KIND_TO_NAME->{$self->{params}->{kind}},
-		$self->{params}->{total_point},
-		Today_and_Now
-	    );
-	} else {
-	    return 0;
-	}
+	$self->{body} = sprintf(
+	    _mail_body(),
+	    $self->{params}->{nickname},
+	    $KIND_TO_NAME->{$self->{params}->{kind}},
+	    $self->{params}->{total_point},
+	    Today_and_Now
+	);
     }
 
     $self->{body} = Jcode::convert($self->{body},'jis');
     $self->{subject} = encode('MIME-Header', $self->{subject});
-    my $cc = 'sakuragakusha+tyuren@gmail.com';
+
     my %mail = (
 	'Content-Type' => 'text/plain; charset="iso-2022-jp"',
 	'From'         => $self->{from_email},
 	'To'           => $self->{to_email},
-	'Cc'           => $cc,
+	'Cc'           => CC,
 	'Subject'      => $self->{subject},
 	'message'      => $self->{body},
     );
 
-    sendmail %mail;
+    return sendmail %mail;
 }
 
 sub _mail_body {
